f0faa9b80103b40080e5ec429b9c12b5be95f336
John Keith <johnkeith@outlook.com>
2014-03-11 21:54:20 -0400

    init commit and push

---
+++ source/js/plugins.js
@@ -0,0 +1,56 @@
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+// Place any jQuery/helper plugins in here.
+(function ($) {
+	var $container = $('.portfolio'),
+		colWidth = function () {
+			var w = $container.width(), 
+				columnNum = 1,
+				columnWidth = 0;
+			if (w > 1200) {
+				columnNum  = 5;
+			} else if (w > 900) {
+				columnNum  = 4;
+			} else if (w > 600) {
+				columnNum  = 3;
+			} else if (w > 300) {
+				columnNum  = 2;
+			}
+			columnWidth = Math.floor(w/columnNum);
+			$container.find('.item').each(function() {
+				var $item = $(this),
+					multiplier_w = $item.attr('class').match(/item-w(\d)/),
+					multiplier_h = $item.attr('class').match(/item-h(\d)/),
+					width = multiplier_w ? columnWidth*multiplier_w[1]-4 : columnWidth-4,
+					height = multiplier_h ? columnWidth*multiplier_h[1]*0.5-4 : columnWidth*0.5-4;
+				$item.css({
+					width: width,
+					height: height
+				});
+			});
+			return columnWidth;
+		},
+		isotope = function () {
+			$container.isotope({
+				resizable: false,
+				itemSelector: '.item',
+				masonry: {
+					columnWidth: colWidth(),
+					gutterWidth: 10
+				}
+			});
+		};
+	isotope();
+	$(window).smartresize(isotope);
+}(jQuery));
